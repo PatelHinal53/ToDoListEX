139a160fbb036c7bd07a45d78a8df59789be08ea
hinal.patel <hinal.patel@1R-79>
2022-02-23 15:23:55 +0530

    Add project files.

---
+++ Todolist/CustomAdapter.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Todolist.Helper;
+
+namespace Todolist
+{
+    class CustomAdapter : BaseAdapter
+    {
+        private MainActivity mainActivity;
+        private List<string> taskList;
+        private DbHelper dbHelper;
+        private ImageView ImageView;
+        public CustomAdapter(MainActivity mainActivity, List<string> taskList, DbHelper dbHelper)
+        {
+            this.mainActivity = mainActivity;
+            this.taskList = taskList;
+            this.dbHelper = dbHelper;
+        }
+        public override int Count { get { return taskList.Count; } }
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return position;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            LayoutInflater inflater = (LayoutInflater)mainActivity.GetSystemService(Context.LayoutInflaterService);
+            View view = inflater.Inflate(Resource.Layout.todo, null);
+            TextView txtTask = view.FindViewById<TextView>(Resource.Id.textView);
+            ImageView ImageView = view.FindViewById<ImageView>(Resource.Id.imageView);
+            txtTask.Text = taskList[position];
+            ImageView.Click += delegate
+            {
+                string task = taskList[position];
+                dbHelper.deleteTask(task);
+                mainActivity.LoadTaskList(); // Reload Data  
+            };
+            return view;
+        }
+    }
+}
---
+++ Todolist/MainActivity.cs
@@ -0,0 +1,75 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using System.Collections.Generic;
+using Todolist.Helper;
+using AlertDialog = Android.App.AlertDialog;
+
+namespace Todolist
+{
+    [Activity(Label = "TodoList", Theme = "@style/Theme.AppCompat.Light", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+       
+        
+        private TextView textView;
+        private ListView listView;
+        DbHelper dbHelper;
+        private CustomAdapter adapter;
+        private EditText editText;
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_item, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.actions:
+                    editText = new EditText(this);
+                    AlertDialog alertDialog = new AlertDialog.Builder(this)
+                        .SetTitle("Add New Task")
+                        .SetView(editText)
+                        .SetPositiveButton("Add", OkAction)
+                        .Create();
+                    alertDialog.Show();
+                    return true;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void OkAction(object sender, DialogClickEventArgs e)
+        {
+            string task = editText.Text;
+            dbHelper.InsertNewTask(task);
+            LoadTaskList();
+        }
+
+        internal void LoadTaskList()
+        {
+            List<string> taskList = dbHelper.getTaskList();
+            adapter = new CustomAdapter(this, taskList, dbHelper);
+            listView.Adapter = adapter;
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource  
+            SetContentView(Resource.Layout.activity_main);
+            dbHelper = new DbHelper(this);
+            listView = FindViewById<ListView>(Resource.Id.listView1);
+            //Load Data  
+            LoadTaskList();
+        }
+
+        
+    }
+}
